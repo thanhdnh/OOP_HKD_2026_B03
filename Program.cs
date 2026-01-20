a43eb1f18519c0891ecba1e467e97b05d068177a
thanhdnh <myhoangthanh@yahoo.com>
2026-01-20 15:54:02 +0700

    OK

--- Program.cs
+++ Program.cs
@@ -11,14 +11,16 @@
     }
     public string GetInfos()
     {
-        return $"Ten: {name}, Sinh nam: {birthday}, Gioi tinh: {(gender?"Nam":"Nu")}";
+        return $"Ten: {name}, Sinh nam: {birthday}, Gioi tinh: {(gender ? "Nam" : "Nu")}";
     }
 }
-public class Employee:Human{
+public class Employee : Human
+{
     private string id;
     private float salarycoef;
     private DateTime participationdate;
-    public Employee(string name, DateTime birthday, bool gender, string id, float salarycoef, DateTime participationdate):base(name, birthday, gender){
+    public Employee(string name, DateTime birthday, bool gender, string id, float salarycoef, DateTime participationdate) : base(name, birthday, gender)
+    {
         this.id = id;
         this.salarycoef = salarycoef;
         this.participationdate = participationdate;
@@ -28,13 +30,63 @@ public class Employee:Human{
         return $"ID: {id}, " + base.GetInfos() + $", Salary Coef: {salarycoef}, Participation date: {participationdate}";
     }
 }
+
 public class Program
 {
-  public static void Main(string[] args)
-  {
-    Human h = new Human("Nam", new DateTime(), true);
-    Employee emp = new Employee("Hoa", new DateTime(), false, "EMP001", 4.5f, new DateTime());
-    Console.WriteLine(h.GetInfos());
-    Console.WriteLine(emp.GetInfos());
-  }
+    public static void Main(string[] args)
+    {
+        /*Human h = new Human("Nam", new DateTime(), true);
+        Employee emp = new Employee("Hoa", new DateTime(), false, "EMP001", 4.5f, new DateTime());
+        Console.WriteLine(h.GetInfos());
+        Console.WriteLine(emp.GetInfos());*/
+
+        Shape rec = new Rectangle(3, 4);
+        Shape round = new Round(3);
+        Console.WriteLine($"Chu vi HCN: {rec.TinhChuVi()}");
+        Console.WriteLine($"Dien tich HCN: {rec.TinhDienTich()}");
+        Console.WriteLine($"Chu vi HT: {round.TinhChuVi()}");
+        Console.WriteLine($"Dien tich HT: {round.TinhDienTich()}");
+
+    }
+}
+
+public abstract class Shape
+{
+    public abstract float TinhChuVi();
+    public abstract float TinhDienTich();
+}
+public class Rectangle : Shape
+{
+    private float w, h;
+    public Rectangle(float w, float h)
+    {
+        this.w = w;
+        this.h = h;
+    }
+    public override float TinhChuVi()
+    {
+        return 2 * (w + h);
+    }
+
+    public override float TinhDienTich()
+    {
+        return w * h;
+    }
+}
+public class Round : Shape
+{
+    private float R;
+    public Round(float R)
+    {
+        this.R = R;
+    }
+    public override float TinhChuVi()
+    {
+        return 2*(float)Math.PI*R;
+    }
+
+    public override float TinhDienTich()
+    {
+        return (float)Math.PI*R*R;
+    }
 }
